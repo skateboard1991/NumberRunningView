899e4285f8fa83d214fbea6883b51219a2d9cf3a
haoxuan.wu <haoxuan.wu@neulion.com.cn>
2019-03-10 21:28:13 +0800

    update datalist

--- app/src/main/java/com/skateboard/numberrunningtextviewtest/MainActivity.kt
+++ app/src/main/java/com/skateboard/numberrunningtextviewtest/MainActivity.kt
@@ -9,15 +9,12 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
-//    private val redDotList = mutableListOf(
-//        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,
-//        13, 14, 15, 16, 17, 18, 19, 20,
-//        21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33
-//    )
-
     private val redDotList = mutableListOf(
-        1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,
+        13, 14, 15, 16, 17, 18, 19, 20,
+        21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33
     )
+
     private val blueDotList = mutableListOf(
         1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11
         , 12, 13, 14, 15, 16
